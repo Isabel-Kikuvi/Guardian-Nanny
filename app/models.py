8eca564bf139ade4c5ec30e6c10a5dfa6b887fb0
Isabel-Kikuvi <isabel.kikuvi@gmail.com>
2023-11-11 19:40:11 +0300

    created profile pages

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 from app import db
 
 @login.user_loader
@@ -13,17 +14,24 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    selected_nannies = db.relationship('NannySelection', back_populates='user')
+    about_me = db.Column(db.String(140))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-    selected_nannies = db.relationship('NannySelection', back_populates='user')
+    
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
     
 # Nanny Model
 class Nanny(db.Model):
